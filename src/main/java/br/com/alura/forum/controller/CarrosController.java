2134ad01dde21de152503dc1a6b917d5880dd0f6
Alex Souza da Silva <44552810+AlexSouzaSilvax@users.noreply.github.com>
2019-08-30 23:20:07 -0300

    Atualizando img (Idea, Polo)

--- src/main/java/br/com/alura/forum/controller/CarrosController.java
+++ src/main/java/br/com/alura/forum/controller/CarrosController.java
@@ -14,8 +14,8 @@ public class CarrosController {
 	@RequestMapping("/carros")	
 	public List<Carros> listaCarros() {		
 
-	    Carros c = new Carros("0", "https://pbs.twimg.com/media/DGqBkDzXUAAwLaL?format=jpg&name=small", "Fiat Idea Attractive", "Prata", "2014", "Fiat", "28.000");
-	    Carros c0 = new Carros("1", "https://pbs.twimg.com/media/EBFImrEWwAIKl0r?format=png&name=small", "VW Polo 1.6 Hatch", "Vermelha", "2003", "Volkswagens", "18.800");
+	    Carros c = new Carros("0", "https://scontent-ort2-1.cdninstagram.com/vp/ffaf9e20d9e62b08a6bccb3cd0aed3c6/5E098503/t51.2885-15/e35/67886062_496588337772960_8760688499312704207_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com", "Fiat Idea Attractive", "Prata", "2014", "Fiat", "28.000");
+	    Carros c0 = new Carros("1", "http://www.carrosdub.com.br/wp-content/uploads/2016/05/polo-rebaixado-aro18-01-5.jpg", "VW Polo 1.6 Hatch", "Vermelha", "2003", "Volkswagens", "18.800");
 		Carros c1 = new Carros("2", "https://i.pinimg.com/originals/de/5e/e8/de5ee8d1de7bfd41e087a83995850776.jpg", "Corolla fixa", "Prata", "2019", "Toyota", "48.900");
 		Carros c2 = new Carros("3", "https://s2.glbimg.com/EKy5hlv4OtZgJDODUvfTdT_mhN8=/620x413/e.glbimg.com/og/ed/f/original/2018/04/24/chevrolet-s10-2019-14.jpg", "Chevrolet S10", "Preta", "2019", "Chevrolet", "108.100");
 		Carros c3 = new Carros("4", "https://img2.icarros.com/dbimg/imgadicionalnoticia/4/103518_1", "Fiat Toro", "Vermelha", "2019", "Fiat", "89.200" );
